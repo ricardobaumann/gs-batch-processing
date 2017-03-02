6f363d8415b239d8dfe3018eabb11917c8ea9ad5
ricardobaumann <ricardo.luis.baumann@gmail.com>
2017-03-02 14:49:18 +0100

    add schedule support

--- complete/src/main/java/hello/BatchConfiguration.java
+++ complete/src/main/java/hello/BatchConfiguration.java
@@ -1,28 +1,36 @@
 package hello;
 
-import javax.sql.DataSource;
-
+import lombok.SneakyThrows;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
+@EnableScheduling
 public class BatchConfiguration {
 
     @Autowired
@@ -33,6 +41,12 @@ public class BatchConfiguration {
 
     @Autowired
     public DataSource dataSource;
+
+    @Autowired
+    private SimpleJobLauncher jobLauncher;
+
+    @Autowired
+    private Job importUserJob;
 
     // tag::readerwriterprocessor[]
     @Bean
@@ -48,6 +62,36 @@ public class BatchConfiguration {
             }});
         }});
         return reader;
+    }
+
+    @Bean
+    public ResourcelessTransactionManager transactionManager() {
+        return new ResourcelessTransactionManager();
+    }
+
+    @Bean
+    public MapJobRepositoryFactoryBean mapJobRepositoryFactory(
+            ResourcelessTransactionManager txManager) throws Exception {
+
+        MapJobRepositoryFactoryBean factory = new
+                MapJobRepositoryFactoryBean(txManager);
+
+        factory.afterPropertiesSet();
+
+        return factory;
+    }
+
+    @Bean
+    public JobRepository jobRepository(
+            MapJobRepositoryFactoryBean factory) throws Exception {
+        return factory.getObject();
+    }
+
+    @Bean
+    public SimpleJobLauncher jobLauncher(JobRepository jobRepository) {
+        SimpleJobLauncher launcher = new SimpleJobLauncher();
+        launcher.setJobRepository(jobRepository);
+        return launcher;
     }
 
     @Bean
@@ -74,6 +118,15 @@ public class BatchConfiguration {
                 .flow(step1())
                 .end()
                 .build();
+    }
+
+    @SneakyThrows
+    @Scheduled(fixedRate = 1000)
+    public void perform() {
+        JobParameters param = new JobParametersBuilder().addString("JobID",
+                String.valueOf(System.currentTimeMillis())).toJobParameters();
+
+        jobLauncher.run(importUserJob,param);
     }
 
     @Bean
